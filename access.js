2c621311d782ab59c3afee8f783580d553f84dfe
Splazzzy <bl00dyskygen@gmail.com>
2025-05-09 11:33:04 -0700

    Update access.js

--- access.js
+++ access.js
@@ -1,9 +1,13 @@
 const validCodes = [
-  "JASONISFOOT",
-  "ANDREWISFOOT",
-  "JAIDENISFOOT",
-  "JORGEISFOOT",
-  "GAVINISFOOT"
+  "3932-8361-9243",
+  "2132-4372-5567",
+  "1237-0128-3242",
+  "9162-1623-9174",
+  "9321-5315-8263",
+  "2321-4312-6424",
+  "1221-8364-1238",
+  "2123-8462-9173",
+  "9382-9471-1240"
 ];
 
 function checkAccess() {
